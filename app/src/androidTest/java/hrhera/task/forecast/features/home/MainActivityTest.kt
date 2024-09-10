b28ef8ef0f47fd7c727b53eccfedae94b1d4189e
Ali hrhera <alihrhera1@gmail.com>
2024-09-10 23:15:37 +0300

    add some testcase for ui

--- app/src/androidTest/java/hrhera/task/forecast/features/home/MainActivityTest.kt
+++ app/src/androidTest/java/hrhera/task/forecast/features/home/MainActivityTest.kt
@@ -1,13 +1,17 @@
 package hrhera.task.forecast.features.home
 
+import android.graphics.Rect
+import android.view.View
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isClickable
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import hrhera.task.forecast.R
+import org.hamcrest.CoreMatchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,5 +29,30 @@ class MainActivityTest {
         onView(withId(R.id.main)).check(matches(isDisplayed()))
     }
 
+    @Test
+    fun checkRetryButtonIsClickable() {
+        onView(withId(R.id.retry)).check(matches(isClickable()));
+    }
+
+    @Test
+    fun checkLoadingIsVisibleClickable() {
+        onView(withId(R.id.cities_loading)).check { view, _ ->
+            val isVisible = view.visibility == View.VISIBLE
+            val rect = Rect()
+            view.getGlobalVisibleRect(rect)
+            println("Visibility: $isVisible, Global Rect: $rect")
+        }
+    }
+
+    @Test
+    fun checkCityIsVisible() {
+        onView(withId(R.id.cityContainer)).check(matches(isDisplayed()))
+    }
+
+
+    @Test
+    fun checkCityIsClickable() {
+        onView(withId(R.id.cities)).check(matches(isClickable()))
+    }
 
 }
