5b0aaff8e225bd13228c6420bfccfaf8d7fba802
Ali hrhera <alihrhera1@gmail.com>
2024-09-11 00:12:08 +0300

    enhance ui

--- app/src/main/java/hrhera/task/forecast/features/home/DaysForecastAdapter.kt
+++ app/src/main/java/hrhera/task/forecast/features/home/DaysForecastAdapter.kt
@@ -3,27 +3,23 @@ package hrhera.task.forecast.features.home
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import hrhera.task.forecast.databinding.ItemDayForecastBinding
 import hrhera.task.forecast.databinding.ItemDayTempsBinding
 import hrhera.task.forecast.domain.weather.DayForecast
 import hrhera.task.forecast.domain.weather.DayTemperature
-import hrhera.task.forecast.utils.formatTime
 import java.time.format.DateTimeFormatter
 
 class DaysForecastAdapter : ListAdapter<DayForecast, DaysForecastViewHolder>(object : DiffUtil.ItemCallback<DayForecast>() {
     override fun areItemsTheSame(oldItem: DayForecast, newItem: DayForecast): Boolean {
-        return oldItem == newItem
-
+        return oldItem.date == newItem.date
     }
 
     override fun areContentsTheSame(oldItem: DayForecast, newItem: DayForecast): Boolean {
-        return oldItem.date == newItem.date
+        return oldItem == newItem
     }
 }) {
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         DaysForecastViewHolder(ItemDayForecastBinding.inflate(LayoutInflater.from(parent.context), parent, false))
@@ -33,43 +29,46 @@ class DaysForecastAdapter : ListAdapter<DayForecast, DaysForecastViewHolder>(obj
         val forecast = getItem(position)
         holder.binding.forecast = forecast
         holder.binding.executePendingBindings()
-        bindDayTemperatures(holder.binding, forecast.dayTemperatures)
-
+        bindDayTemperatures(
+            binding = holder.binding, dayTemperatures = forecast.dayTemperatures
+        )
     }
 
-    private fun bindDayTemperatures(binding: ItemDayForecastBinding, dayTemperatures: List<DayTemperature>) {
+    /**
+     * Bind the day temperatures to the day forecast layout.
+     *
+     * @param binding the binding of the item day forecast
+     * @param dayTemperatures the list of day temperatures
+     */
+    private fun bindDayTemperatures(
+        binding: ItemDayForecastBinding,
+        dayTemperatures: List<DayTemperature>,
+    ) {
         binding.layDayTemps.removeAllViews()
-        if (dayTemperatures.size > 3) {
-
-            dayTemperatures.firstOrNull()?.let {
-                addViewToDayLayout(it, binding)
-            }
-            if ((dayTemperatures.size / 2) + 1 in dayTemperatures.indices) {
-                addViewToDayLayout(dayTemperatures[dayTemperatures.size / 2], binding)
-                addViewToDayLayout(dayTemperatures[(dayTemperatures.size / 2) + 1], binding)
-            }
-            dayTemperatures.lastOrNull()?.let {
-                addViewToDayLayout(it, binding, false)
-            }
-        } else {
-            dayTemperatures.forEachIndexed { index, dayTemperature ->
-                addViewToDayLayout(dayTemperature, binding, index != dayTemperatures.lastIndex)
-            }
+        dayTemperatures.forEach { dayTemperature ->
+            addViewToDayLayout(
+                dayTemperature = dayTemperature,
+                binding = binding,
+            )
         }
     }
 
+    /**
+     * Add a view to the day layout for the given [dayTemperature].
+     *
+     * @param dayTemperature the day temperature to add a view for
+     * @param binding the binding of the item day forecast
+     */
     private fun addViewToDayLayout(
-        it: DayTemperature,
+        dayTemperature: DayTemperature,
         binding: ItemDayForecastBinding,
-        showLine: Boolean = true
     ) {
         val view = ItemDayTempsBinding.inflate(LayoutInflater.from(binding.root.context), binding.layDayTemps, false)
-        view.textTime.text = it.time.formatTime()
+        view.textTime.text = dayTemperature.time.format(DateTimeFormatter.ofPattern("HH:mm"))
 
-        view.txtTemperature.text = it.temperature
-        view.line.isVisible = showLine
+        view.txtTemperature.text = dayTemperature.temperature
         view.root.layoutParams =
-            LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+            LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
         binding.layDayTemps.addView(view.root)
     }
 }
